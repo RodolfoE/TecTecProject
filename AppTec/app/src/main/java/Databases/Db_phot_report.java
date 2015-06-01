a6dc1edb374b5a564caf8cee2c0a28ab015199b8
Rodolfo Eliezer <rodolfo_eliezer@hotmail.com>
2015-06-01 20:19:22 -0300

    updates

--- AppTec/app/src/main/java/Databases/Db_phot_report.java
+++ AppTec/app/src/main/java/Databases/Db_phot_report.java
@@ -11,21 +11,39 @@ import db_Lines.PhotReport;
  */
 public class Db_phot_report {
     public static final String DB_NAME = Db_employees.DB_NAME;
-    public static final String TABLE_NAME= "Photo report";
-    public static final String COLUMN_NAME_ENTERPRISE = "Name enterprise";
-    public static final String COLUMN_PAT_NUMBER = "Name enterprise";
-    public static final String COLUMN_REPORT_NUMBER = "Name enterprise";
 
-    SQLiteDatabase mDatabase;
+    SQLiteDatabase mDb_enterprises;
+
+    public static final String TABLE_ENTERPRISES = "report";
+    public static final String COLUMN_NAME_ENTERPRISE = "Name_enterprise";
+
+    public static final String TABLE_REPORTS = "reports";
+    public static final String COLUMN_PAT_NUMBER = "Pat_number";
+    public static final String COLUMN_REPORT_NUMBER = "Name_enterprise";
+    public static final String CODE_ENTERPRISE = "codeEnterprise";
+
+    public static final String TABLE_PHOTOS = "PathToPhotos";
+    public static final String COLUMN_PATH = "reportPaths";
+    public static final String CODE_REPORT = "codigoRelatorio";
     Context mCtx;
 
     public Db_phot_report(Context ctx){
         this.mCtx = ctx;
-        mDatabase = ctx.openOrCreateDatabase(DB_NAME, Context.MODE_PRIVATE, null);
-        mDatabase.execSQL("CREATE TABLE IF NOT EXISTS " + TABLE_NAME +
-                " ( " + COLUMN_NAME_ENTERPRISE + " varchar(100)," +
-                " ( " + COLUMN_REPORT_NUMBER + " varchar(100), " +
-                " ( " + COLUMN_PAT_NUMBER + " varchar(100) )" );
+
+        mDb_enterprises = ctx.openOrCreateDatabase(DB_NAME, Context.MODE_PRIVATE, null);
+
+        mDb_enterprises.execSQL("CREATE TABLE IF NOT EXISTS " + TABLE_ENTERPRISES +
+                " ( '" + COLUMN_NAME_ENTERPRISE + "' varchar(100), " +
+                " '" + CODE_ENTERPRISE + "' ," +
+                "Primary key('" + COLUMN_NAME_ENTERPRISE + "' );");
+
+        mDb_enterprises.execSQL("CREATE TABLE IF NOT EXISTS " + TABLE_REPORTS +
+                " ( '" + COLUMN_REPORT_NUMBER + "' varchar(100), '" +
+                 COLUMN_PAT_NUMBER + "' varchar(100) );");
+
+        mDb_enterprises.execSQL("CREATE TABLE IF NOT EXISTS " + TABLE_PHOTOS  + " ('" + COLUMN_PATH + "' varchar(200)," +
+                "'" + CODE_REPORT);
+
     }
 
     public boolean addReport(PhotReport photReport){
@@ -33,7 +51,11 @@ public class Db_phot_report {
         values.put(COLUMN_NAME_ENTERPRISE, photReport.getmNomeEmpresa());
         values.put(COLUMN_REPORT_NUMBER, photReport.getmNumOrdemManutencao());
         values.put(COLUMN_PAT_NUMBER, photReport.getmNumPat());
-        mDatabase.insert(TABLE_NAME, null, values);
+        mDb_enterprises.insert(TABLE_ENTERPRISES, null, values);
+        return true;
+    }
+
+    public boolean deleteReport(){
         return true;
     }
 
